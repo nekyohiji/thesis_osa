0d35dcff88b0eacfe7a973381dbc62a16665054d
josheru27 <jochellearon@gmail.com>
2025-06-09 21:50:23 +0800

    Add new files

--- myapp/views.py
+++ myapp/views.py
@@ -4,4 +4,10 @@ from django.shortcuts import render
 def index(request):
     return HttpResponse("🎓 Hello from your thesis web app!")
 def home_view(request):
-    return render(request, 'myapp/home.html')
+    return render(request, 'myapp/client_home.html')
+def login_view(request):
+    return render (request, 'myapp/login.html')
+def guard_violation_view(request):
+    return render (request, 'myapp/guard_violation.html')
+def guard_report_view(request):
+    return render (request, 'myapp/guard_report.html')
