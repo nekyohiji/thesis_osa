2e7acb69608b88c8bc1f6dd9054b0edb60948b01
josheru27 <jochellearon@gmail.com>
2025-06-09 21:42:21 +0800

    Add new templates and CSS files; update views and settings

--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,7 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
     return HttpResponse("🎓 Hello from your thesis web app!")
+def home_view(request):
+    return render(request, 'myapp/home.html')
