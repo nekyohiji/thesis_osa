5439abb59bf61e765fdae6751811370cce1f3ebd
josheru27 <jochellearon@gmail.com>
2025-06-11 04:24:59 +0800

    message

--- myapp/urls.py
+++ myapp/urls.py
@@ -13,6 +13,23 @@ urlpatterns = [
     path('client_studentAssistantship/', views.client_studentAssistantship_view, name='client_studentAssistantship'),
     path('client_ACSO/', views.client_ACSO_view, name='client_ACSO'),
     path('client_lostandfound/', views.client_lostandfound_view, name='client_lostandfound'),
+    path('admin_dashboard/', views.admin_dashboard_view, name='admin_dashboard'),
+    path('admin_accounts/', views.admin_accounts_view, name='admin_accounts'),
+    path('admin_ackreq/', views.admin_ackreq_view, name='admin_ackreq'),
+    path('admin_ACSO/', views.admin_ACSO_view, name='admin_ACSO'),
+    path('admin_assistantship/', views.admin_assistantship_view, name='admin_assistantship'),
+    path('admin_CS/', views.admin_CS_view, name='admin_CS'),
+    path('admin_election/', views.admin_election_view, name='admin_election'),
+    path('admin_goodmoral/', views.admin_goodmoral_view, name='admin_goodmoral'),
+    path('admin_lostandfound/', views.admin_lostandfound_view, name='admin_lostandfound'),
+    path('admin_report/', views.admin_report_view, name='admin_report'),
+    path('admin_scholarships/', views.admin_scholarships_view, name='admin_scholarships'),
+    path('admin_view_ackreq/', views.admin_view_ackreq_view, name='admin_view_ackreq'),
+    path('admin_view_CS/', views.admin_view_CS_view, name='admin_view_CS'),
+    path('admin_view_goodmoral/', views.admin_view_goodmoral_view, name='admin_view_CS'),
+    path('admin_view_violation/', views.admin_view_violation_view, name='admin_view_violation'),
+    path('admin_violation/', views.admin_violation_view, name='admin_violation'),
     
-   
+    
+    
 ]
