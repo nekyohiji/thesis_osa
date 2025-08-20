da56d978e42ebff43594eba116586a064c171550
nekyohiji <lordjaissonriberal04@gmail.com>
2025-08-20 18:46:45 +0800

    latest update

--- myapp/urls.py
+++ myapp/urls.py
@@ -129,8 +129,10 @@ urlpatterns = [
 
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-# Prod: explicit media route (works with DEBUG=False)
-urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
-  + [re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT})]
-
+# Prod (Render): explicitly serve media even when DEBUG=False
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
